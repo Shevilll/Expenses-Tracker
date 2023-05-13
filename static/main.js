cd915894cf44adc8ab4f0337dd850202b04db1b1
Shevilll <ahmadfaraz00710@gmail.com>
2023-05-13 12:46:49 +0530

    Functions for remove

--- static/main.js
+++ static/main.js
@@ -2,10 +2,21 @@ const homepage = document.getElementById("HomePage");
 homepage.style.display = "block";
 
 function displaybuttonformdiv(value) {
-    const buttonformdiv = document.querySelectorAll(".button-form-div");
-    const buttonfordiv = document.querySelector(".buttonfordiv");
+    const buttonformdiv = document.querySelectorAll("#button-form-div");
+    const buttonfordiv = document.querySelector("#buttonfordiv");
+    const buttonforremovediv = document.querySelector("#buttonforremovediv");
+    const buttoncheckeditems = document.querySelector("#buttoncheckeditems");
+
     buttonformdiv.forEach((button) => {
         button.style.display = value;
-        console.log(button);
     });
+    if (value == "none") {
+        buttonfordiv.style.display = "block";
+        buttonforremovediv.style.display = "none";
+        buttoncheckeditems.style.display = "none";
+    } else {
+        buttonfordiv.style.display = "none";
+        buttonforremovediv.style.display = "block";
+        buttoncheckeditems.style.display = "block";
+    }
 }
